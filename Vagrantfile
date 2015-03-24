f093baf2f19bbbc337068f4658086541902f1a7c
Dan Nuttle <dnuttle@adobe.com>
2015-03-24 17:18:20 -0500

    Changes

--- Vagrantfile
+++ Vagrantfile
@@ -1,9 +1,20 @@
-Vagrant::Config.run do |config|
+#Vagrant::Config.run do |config|
+#  config.vm.box = "precise64"
+#
+#  config.vm.network :hostonly, "192.168.3.101"
+#  config.vm.host_name = "solrlab"
+#
+#  config.vm.provision :shell, path: "provision.sh"
+#
+#end
+
+Vagrant.configure(2) do |config|
   config.vm.box = "precise64"
 
-  config.vm.network :hostonly, "192.168.3.101"
-  config.vm.host_name = "solr1"
-
-  config.vm.provision :shell, path: "provision.sh"
-
+  config.vm.network :private_network, ip: "192.168.3.101"
+  config.vm.network :forwarded_port, host:8984, guest:8983
+  config.vm.network :forwarded_port, host:8985, guest:8984
+  config.vm.host_name = "solrlab"
+  config.vm.provision "shell", path: "./provision.sh"
+  #config.vm.provision "shell", path: "./start-solr.sh", run: "always"
 end
--- provision.sh
+++ provision.sh
@@ -1,4 +1,4 @@
-﻿#!/bin/bash
+#!/bin/bash
 sudo apt-get update >/dev/null
 sudo apt-get -y install curl >/dev/null
 sudo apt-get -y install wget >/dev/null
@@ -11,12 +11,14 @@ echo "download zookeeper"
 wget --progress=dot http://apache.mirrors.lucidnetworks.net/zookeeper/zookeeper-3.4.6/zookeeper-3.4.6.tar.gz >/dev/install 2&>1
 echo "untar zookeeper"
 tar xzf zookeeper-3.4.6.tar.gz >/dev/install 2&>1
+sudo chown vagrant:vagrant zookeeper-3.4.6
 
 echo "download solr"
 wget --progress=dot http://apache.cs.utah.edu/lucene/solr/4.10.2/solr-4.10.2.tgz >/dev/null 2&>1
 sudo chown vagrant:vagrant solr-4.10.2.tgz
 echo "untar solr"
 tar xzf solr-4.10.2.tgz
+sudo chown vagrant:vagrant solr-4.10.2
 
 if [ ! -e "/etc/solr" ]; then
 	sudo mkdir /etc/solr
@@ -25,6 +27,8 @@ fi
 if [ ! -e "/etc/solr/data" ]; then
 	sudo mkdir /etc/solr/data
 	sudo chown vagrant:vagrant /etc/solr/data
+	sudo mkdir /etc/solr/data/
+	sudo chown /
 fi
 if [ ! -e "/etc/solr/zk_data" ]; then
 	sudo mkdir /etc/solr/zk_data
